0ce399d60dc6b1b56455eaa3d427dd3d9e7f03c8
Minh Nghĩa <nghiaphamln3@gmail.com>
2021-11-14 18:48:53 +0700

    edit crawl.py

--- crawl.py
+++ crawl.py
@@ -28,13 +28,16 @@ if __name__ == "__main__":
                 'giao-duc-huong-nghiep', 'an-sinh', 'phap-luat', 'photo-story', 'du-lich', 'tinh-yeu-gioi-tinh',
                 'khoa-hoc-cong-nghe', 'ban-doc', 'doi-song', 'nhip-song-tre', 'blog', 'emagazine', 'infographic']
     for _category in category:
-        for page in range(1, 31):
-            # cào dữ liệu lưu vào file
-            print('Crawling Catagory {} - Page {}'.format(_category, page))
-            respone = requests.get("{}/{}/trang-{}.htm".format(url, _category, page))
-            soup = BeautifulSoup(respone.text, 'html.parser')
-            for child_url in soup.find_all('h3', class_='news-item__title'):
-                filename = str(uuid.uuid4())
-                print('[{}] - {}'.format(count, child_url.a['href']))
-                crawl_content(url + child_url.a['href'], "./data_crawl/{}.story".format(filename))
-                count += 1
+        try:
+            for page in range(1, 31):
+                # cào dữ liệu lưu vào file
+                print('Crawling Catagory {} - Page {}'.format(_category, page))
+                respone = requests.get("{}/{}/trang-{}.htm".format(url, _category, page))
+                soup = BeautifulSoup(respone.text, 'html.parser')
+                for child_url in soup.find_all('h3', class_='news-item__title'):
+                    filename = str(uuid.uuid4())
+                    print('[{}] - {}'.format(count, child_url.a['href']))
+                    crawl_content(url + child_url.a['href'], "./data_crawl/{}.story".format(filename))
+                    count += 1
+        except Exception as e:
+            print(e)
